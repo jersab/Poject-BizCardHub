b5d35b3470a40ec15eec9b68271e8017d72d465b
jersab <ajersab@gmail.com>
2025-03-23 09:05:20 +0200

    new1

--- src/components/cards/Bcard.tsx
+++ src/components/cards/Bcard.tsx
@@ -118,7 +118,7 @@ const Bcard: FunctionComponent<BcardProps> = ({ card, onLikeChange, onDelete }) 
             <img className="card-img-top" src={card.image.url} alt={card.image.alt} />
             <div className="card-body">
                 <h5 className="card-title">{card.title}</h5>
-                <h5 className="card-subtitle mb-2 text-muted">{card.subtitle}</h5>
+                <h6 className="card-subtitle mb-2">{card.subtitle}</h6>
                 <p className="card-text text-truncate">{card.description}</p>
             </div>
             <ul className="list-group list-group-flush">
